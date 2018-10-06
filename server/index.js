30d40707ff08dfb891b2c70babcea7f5f06d6b7f
TruongTX <me@truongtx.me>
2018-10-06 10:58:00 +0700

    Add static layer for backend

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const koaStatic = require('koa-static');
 
 const config = require('./config');
 const apiRouter = require('./apis');
 
 const app = new Koa();
+app.use(koaStatic('./static'));
 app.use(bodyParser());
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
 
