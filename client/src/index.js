365b78ca0b8964a507b14c517ed968918da48463
TruongTX <me@truongtx.me>
2018-10-07 16:56:44 +0700

    Add lato and font awesome

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/scss/font-awesome.scss';
+import 'lato-font/scss/lato-font.scss';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
