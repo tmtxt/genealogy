d6bb13cdc25984bb574db639844cef71efbb4aab
TruongTX <me@truongtx.me>
2018-10-07 17:55:50 +0700

    Add person detail page

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import HomePage from './components/home-page';
+import HomePage from 'components/home-page';
+import PersonDetailPage from 'components/person-detail-page';
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/persons/:personId" component={PersonDetailPage} />
     </div>
   </Router>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
 );
 
 export default App;
---
+++ client/src/components/person-detail-page/index.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import MainLayout from '../layouts/main';
+
+class PersonDetailPage extends Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default PersonDetailPage;
