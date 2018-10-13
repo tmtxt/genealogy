11be30531b1d23b90a8b72dde2655b7ae9647cb5
TruongTX <me@truongtx.me>
2018-10-13 15:53:28 +0700

    Pick only necessary props

--- server/models/base.js
+++ server/models/base.js
@@ -3,9 +3,17 @@
 const _ = require('lodash');
 const validate = require('jsonschema').validate;
 
+const pickPropertiesFromSchema = (props, schema) => {
+  if (schema.type !== 'object' || !schema.properties) return props;
+
+  const keys = Object.keys(schema.properties);
+  return _.pick(props, keys);
+};
+
 class ModelBase {
   constructor(props) {
     const Class = this.constructor;
+    props = pickPropertiesFromSchema(props, Class.schema);
     const res = validate(props, Class.schema);
 
     if (!_.isEmpty(res.errors)) {
