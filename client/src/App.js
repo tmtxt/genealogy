53284af11fa13488442dc4af7b60a1ba6d273ebe
TruongTX <me@truongtx.me>
2018-10-16 20:03:01 +0700

    Add Alert Provider to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
 import 'libs/moment';
 
@@ -12,15 +14,23 @@ import TreePage from 'components/page-tree';
 import AddChildPage from 'components/page-add-child';
 
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/persons/:personId/edit" component={PersonEditPage} />
-      <Route exact path="/persons/:personId" component={PersonDetailPage} />
-      <Route exact path="/persons/:personId/add-child" component={AddChildPage} />
-      <Route path="/tree" component={TreePage} />
-    </div>
-  </Router>
+  <AlertProvider
+    template={AlertTemplate}
+    position="bottom center"
+    timeout={5000}
+    offset="30px"
+    transition="scale"
+  >
+    <Router>
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/persons/:personId/edit" component={PersonEditPage} />
+        <Route exact path="/persons/:personId" component={PersonDetailPage} />
+        <Route exact path="/persons/:personId/add-child" component={AddChildPage} />
+        <Route path="/tree" component={TreePage} />
+      </div>
+    </Router>
+  </AlertProvider>
 );
 
 export default wrapContextProviders(App);
