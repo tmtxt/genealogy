4c2461cb4d234072f4287f680fa670759036b799
TruongTX <me@truongtx.me>
2018-10-26 19:41:34 +0700

    Update env for debug

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   port: parseInt(process.env.BACKEND_PORT) || 80,
   ensureDataEnabled: process.env.ENSURE_DATA_ENABLED === 'true',
-  logLevel: process.env.LOG_LEVEL,
+  logLevel: process.env.LOG_LEVEL || 'info',
   // for cookie
   secretKey: process.env.SECRET_KEY || 'secret-key',
   // for user auth
