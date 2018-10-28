220f5f046853854ff37920472584511e88ed293f
TruongTX <me@truongtx.me>
2018-10-28 10:53:46 +0700

    Handle on change on child order

--- client/src/components/page-edit-children-order/edit-children-order-page.jsx
+++ client/src/components/page-edit-children-order/edit-children-order-page.jsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Loader } from '../shared';
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
-export const EditChildrenOrderPage = ({ childrenInfo }) => {
-  if (!childrenInfo) {
-    return <Loader />;
+export class EditChildrenOrderPage extends Component {
+  state = {
+    childrenInfo: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (!state.childrenInfo && props.childrenInfo) {
+      return {
+        childrenInfo: props.childrenInfo
+      };
+    }
+
+    return null;
   }
 
-  if (!childrenInfo.get('children').size) {
-    return <div>Không có con</div>;
-  }
+  /**
+   * Update the child order in the state
+   * @param {int} childId
+   * @param {int} childOrder
+   */
+  handleChildOrderUpdate = (childId, childOrder) => {
+    childOrder = parseInt(childOrder);
+    if (!childOrder || childOrder < 1) {
+      childOrder = 1;
+    }
 
-  return <div />;
-};
+    const { childrenInfo } = this.state;
+    const children = childrenInfo
+      .get('children')
+      .map(child => (child.get('id') === childId ? child.set('order', childOrder) : child));
+    this.setState({
+      childrenInfo: childrenInfo.set('children', children)
+    });
+  };
+
+  render() {
+    let { childrenInfo } = this.state;
+
+    if (!childrenInfo) {
+      return <Loader />;
+    }
+
+    const children = childrenInfo.get('children');
+    if (!children.size) {
+      return <div>Không có con</div>;
+    }
+
+    return (
+      <Form>
+        {children.map(child => (
+          <FormGroup key={child.get('id')}>
+            <Label>{child.get('name') || 'Không rõ tên'}</Label>
+            <Input
+              type="number"
+              value={child.get('order')}
+              onChange={e => this.handleChildOrderUpdate(child.get('id'), e.target.value)}
+            />
+          </FormGroup>
+        ))}
+
+        <Button color="primary">Cập nhật</Button>
+      </Form>
+    );
+  }
+}
 
 export default EditChildrenOrderPage;
