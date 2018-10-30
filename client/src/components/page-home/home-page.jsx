a85bf9e87f4b0a9310946a8465709c60cdebb16b
TruongTX <me@truongtx.me>
2018-10-30 20:32:53 +0700

    Add more images

--- client/src/components/page-home/home-page.jsx
+++ client/src/components/page-home/home-page.jsx
@@ -9,12 +9,14 @@ import styles from './style.scss';
 import img1 from './car1.jpg';
 import img2 from './car2.jpg';
 import img3 from './car3.jpg';
+import img4 from './car4.jpg';
+import img5 from './car5.jpg';
 
 const items = [
   {
     src: img1,
-    altText: 'Tảo mộ tết 2013',
-    caption: 'Mộ phần chi trưởng họ Trần Văn',
+    altText: 'Phần mộ chi trưởng',
+    caption: 'Mộ phần chi trưởng họ Trần Văn sau cải tạo',
     header: 'Tảo mộ 2013'
   },
   {
@@ -28,6 +30,18 @@ const items = [
     altText: 'Thăm mộ bà Hoàng Thị Loan',
     caption: 'Các cụ trong họ thăm mộ bà Hoàng Thị Loan, thân sinh chủ tịch Hồ Chí Minh',
     header: 'Thăm mộ bà Hoàng Thị Loan'
+  },
+  {
+    src: img4,
+    altText: 'Con cháu bà Trần Thị Tứ',
+    caption: 'Bà Trần Thị Tứ, cùng con cháu ở Phan Thiết',
+    header: 'Con cháu bà Trần Thị Tứ'
+  },
+  {
+    src: img5,
+    altText: 'Mừng thọ ông bà Trần Văn Phương',
+    caption: 'Mừng thọ Ông bà Phương 80 tuổi năm 2009',
+    header: 'Mừng thọ ông bà Trần Văn Phương'
   }
 ];
 
