927d2b5ef9238f791562c366d264e4fb33222fc6
TruongTX <me@truongtx.me>
2018-11-04 11:13:52 +0700

    Add offset for the diagram

--- client/src/components/page-persons-relation/persons-relation-page.jsx
+++ client/src/components/page-persons-relation/persons-relation-page.jsx
@@ -6,16 +6,18 @@ const diagonal = d3.svg.diagonal().projection(d => [d.x, d.y]);
 
 const linkWidth = 100;
 const linkHeight = 150;
+const xOffset = 80;
+const yOffset = 80;
 
 const RelationLink = ({ rel }) => {
   const data = {
     source: {
-      x: rel.get('startWidth') * linkWidth,
-      y: rel.get('startHeight') * linkHeight
+      x: rel.get('startWidth') * linkWidth + xOffset,
+      y: rel.get('startHeight') * linkHeight + yOffset
     },
     target: {
-      x: rel.get('endWidth') * linkWidth,
-      y: rel.get('endHeight') * linkHeight
+      x: rel.get('endWidth') * linkWidth + xOffset,
+      y: rel.get('endHeight') * linkHeight + yOffset
     }
   };
   const d = diagonal(data);
@@ -34,7 +36,7 @@ const RelationLink = ({ rel }) => {
 
 const PersonsRelationPage = ({ width, height, initialHeight, path }) => {
   return (
-    <svg width={width * linkWidth + 200} height={height * linkHeight}>
+    <svg width={width * linkWidth + 2 * xOffset} height={height * linkHeight * 2 * yOffset}>
       {path.map((rel, i) => (
         <RelationLink key={i} rel={rel} />
       ))}
