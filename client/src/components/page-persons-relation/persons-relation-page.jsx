e6426ee57962b5bc711d25c4a41d9be8b870319a
TruongTX <me@truongtx.me>
2018-11-04 11:12:09 +0700

    Draw basic path from to persons

--- client/src/components/page-persons-relation/persons-relation-page.jsx
+++ client/src/components/page-persons-relation/persons-relation-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import d3 from 'd3';
-import {List as ImmutableList} from 'immutable';
+import { List as ImmutableList } from 'immutable';
 
 const diagonal = d3.svg.diagonal().projection(d => [d.x, d.y]);
 
@@ -8,17 +8,36 @@ const linkWidth = 100;
 const linkHeight = 150;
 
 const RelationLink = ({ rel }) => {
-  console.log(rel.toJS());
+  const data = {
+    source: {
+      x: rel.get('startWidth') * linkWidth,
+      y: rel.get('startHeight') * linkHeight
+    },
+    target: {
+      x: rel.get('endWidth') * linkWidth,
+      y: rel.get('endHeight') * linkHeight
+    }
+  };
+  const d = diagonal(data);
 
-  return <text>a</text>;
+  return (
+    <path
+      d={d}
+      style={{
+        fill: 'none',
+        stroke: '#ccc',
+        strokeWidth: '1.5px'
+      }}
+    />
+  );
 };
 
 const PersonsRelationPage = ({ width, height, initialHeight, path }) => {
   return (
     <svg width={width * linkWidth + 200} height={height * linkHeight}>
-      {path.forEach((rel, i) => {
-
-      })}
+      {path.map((rel, i) => (
+        <RelationLink key={i} rel={rel} />
+      ))}
     </svg>
   );
 };
