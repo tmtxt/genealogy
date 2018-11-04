b3e69731b014cc5399514dae6129de226cb4f90c
TruongTX <me@truongtx.me>
2018-11-04 11:07:43 +0700

    Compute position for path

--- client/src/components/page-persons-relation/persons-relation-page-wrapper.jsx
+++ client/src/components/page-persons-relation/persons-relation-page-wrapper.jsx
@@ -4,16 +4,80 @@ import { connect } from 'react-redux';
 import { fetchPersonsRelation } from 'store/actions/persons-relation';
 import { selectPersonsRelation } from 'store/selectors/persons-relation';
 import PersonsRelationPage from './persons-relation-page';
+import { Loader } from '../shared';
 
 class PersonsRelationPageWrapper extends Component {
   componentDidMount() {
     this.props.fetchPersonsRelation(0, 24);
   }
 
-  render() {
-    
+  computeGraphWidth() {
+    const { path } = this.props;
+    return path.size;
+  }
 
-    return <PersonsRelationPage />;
+  computeGraphHeight() {
+    const { path } = this.props;
+
+    let min = 0;
+    let max = 0;
+    let current = 0;
+
+    path.forEach(rel => {
+      const type = rel.get('type');
+
+      if (type === 'Father_child' || type === 'Mother_child') {
+        current++;
+      } else if (type === 'Child_father' || type === 'Child_mother') {
+        current--;
+      }
+
+      if (current > max) max = current;
+      if (current < min) min = current;
+
+      return rel;
+    });
+
+    return {
+      height: max - min,
+      initialHeight: 0 - min,
+      path
+    };
+  }
+
+  computePathWithPosition = (initialHeight, path) => {
+    let startHeight = initialHeight;
+    let startWidth = 0;
+
+    return path.map(rel => {
+      const type = rel.get('type');
+
+      rel = rel.set('startHeight', startHeight).set('startWidth', startWidth);
+
+      startWidth++;
+
+      if (type === 'Father_child' || type === 'Mother_child') {
+        startHeight++;
+      } else if (type === 'Child_father' || type === 'Child_mother') {
+        startHeight--;
+      }
+
+      rel = rel.set('endHeight', startHeight).set('endWidth', startWidth);
+
+      return rel;
+    });
+  };
+
+  render() {
+    const { path } = this.props;
+
+    if (!path) return <Loader />;
+
+    const width = this.computeGraphWidth();
+    const { height, initialHeight } = this.computeGraphHeight();
+    const updatedPath = this.computePathWithPosition(initialHeight, path);
+
+    return <PersonsRelationPage {...{ width, height, initialHeight, path: updatedPath }} />;
   }
 }
 
--- client/src/components/page-persons-relation/persons-relation-page.jsx
+++ client/src/components/page-persons-relation/persons-relation-page.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import d3 from 'd3';
+import {List as ImmutableList} from 'immutable';
 
-const PersonsRelationPage = props => {
-  return <div> hello</div>;
+const diagonal = d3.svg.diagonal().projection(d => [d.x, d.y]);
+
+const linkWidth = 100;
+const linkHeight = 150;
+
+const RelationLink = ({ rel }) => {
+  console.log(rel.toJS());
+
+  return <text>a</text>;
+};
+
+const PersonsRelationPage = ({ width, height, initialHeight, path }) => {
+  return (
+    <svg width={width * linkWidth + 200} height={height * linkHeight}>
+      {path.forEach((rel, i) => {
+
+      })}
+    </svg>
+  );
 };
 
 export default PersonsRelationPage;
