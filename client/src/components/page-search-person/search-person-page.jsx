bf172c81f4b6068f203669b870d25830f9337774
TruongTX <me@truongtx.me>
2018-11-10 17:08:06 +0700

    Handle click on search result page

--- client/src/components/page-search-person/search-person-page.jsx
+++ client/src/components/page-search-person/search-person-page.jsx
@@ -1,43 +1,91 @@
 // @flow
 import React from 'react';
-import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import {
+  Button,
+  Card,
+  CardText,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  ListGroup,
+  ListGroupItem,
+  Row
+} from 'reactstrap';
 import { connect } from 'react-redux';
 import { Map as ImmutableMap } from 'immutable';
 
 import { findPersonByName } from 'store/actions/person';
 import { selectPersonSearchResults } from 'store/selectors/person-search-results';
 import Loader from 'components/shared/loader';
+import { personDetailUrl, navigateToPersonDetailPage } from 'libs/navigation';
+import { withRouter } from 'react-router-dom';
 
 type Props = {
   searchKey: string,
   onSearchKeyChange: string => void,
   findPersonByName: typeof findPersonByName,
-  searchResults: ImmutableMap<string, any>
+  searchResults: ImmutableMap<string, any>,
+  history: Object
 };
 
 const SearchPersonPage = (props: Props) => {
-  const { searchKey, onSearchKeyChange, findPersonByName, searchResults } = props;
+  const { searchKey, onSearchKeyChange, findPersonByName, searchResults, history } = props;
 
   const isLoaded = searchResults.get('isLoaded');
   const isLoading = searchResults.get('isLoading');
   const results = searchResults.get('results');
 
   return (
-    <Form>
-      <FormGroup>
-        <Label>Tên</Label>
-        <Input
-          type="text"
-          placeholder="Tên"
-          value={searchKey}
-          onChange={(e: Object) => onSearchKeyChange(e.target.value)}
-        />
-      </FormGroup>
-      <Button onClick={() => findPersonByName(searchKey)} color="primary">
-        Tìm kiếm
-      </Button>
-      {isLoading && <Loader />}
-    </Form>
+    <div>
+      <Row>
+        <Col>
+          <Form>
+            <FormGroup>
+              <Label>Tên</Label>
+              <Input
+                type="text"
+                placeholder="Tên"
+                value={searchKey}
+                onChange={(e: Object) => onSearchKeyChange(e.target.value)}
+              />
+            </FormGroup>
+            <Button onClick={() => findPersonByName(searchKey)} color="primary">
+              Tìm kiếm
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 10 }}>
+        <Col>
+          {isLoading && <Loader />}
+          {isLoaded &&
+            !results.size && (
+              <Card body outline color="danger">
+                <CardText>Không tìm thấy</CardText>
+              </Card>
+            )}
+          {isLoaded &&
+            !!results.size && (
+              <ListGroup flush>
+                {results.map(person => (
+                  <ListGroupItem
+                    tag="a"
+                    href={personDetailUrl.stringify({ personId: person.get('id') })}
+                    onClick={(e: Object) => {
+                      e.preventDefault();
+                      navigateToPersonDetailPage(history, person.get('id'));
+                    }}
+                  >
+                    {person.get('name') || 'Không rõ'}
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            )}
+        </Col>
+      </Row>
+    </div>
   );
 };
 
@@ -51,4 +99,4 @@ const mapDispatchToProps = { findPersonByName };
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SearchPersonPage);
+)(withRouter(SearchPersonPage));
